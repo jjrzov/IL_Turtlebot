e53099ce05bd82c7b68ea2fda2a7349fc52a4077
jjrzov <jjrzov@gmail.com>
2025-02-15 23:15:21 -0800

    Cleaned up repo

--- ros_ws/build/lfd/share/lfd/hook/pythonpath_develop.ps1
+++
@@ -1,3 +0,0 @@
-# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
-
-colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/IL_Turtlebot/ros_ws/build/lfd"
--- ros_ws/build/lfd/share/lfd/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/IL_Turtlebot/ros_ws/build/lfd"
